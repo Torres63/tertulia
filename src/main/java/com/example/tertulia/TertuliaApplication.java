94130744434b1ffe5b9351846c5500ee6d0e99c5
bjaguilar <torresbrandon957@gmail.com>
2024-08-20 15:49:24 -0600

    First commit

---
+++ src/main/java/com/example/tertulia/TertuliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tertulia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TertuliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TertuliaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tertulia/TertuliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tertulia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TertuliaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
